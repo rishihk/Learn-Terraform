1d64246db2391628965e8f8571514b94ec1edd98
rishihk <hk.hrishi30@gmail.com>
2024-04-07 03:10:47 -0500

    feat: setup the provider block

---
+++ provider.tf
@@ -0,0 +1,17 @@
+# Part 1: create the required_providers, provider block. (terraform init will download the provider plugin)
+
+# Step 1: create the terraform required_providers block which specifies the required provider and its version.
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Step 2: create the provider block which specifies the provider configuration.
+provider "aws" {
+  region = "us-east-1"
+}
+
