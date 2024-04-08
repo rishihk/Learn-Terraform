1836db5d1ffe240f845cf26b272d8b147741e7f9
rishihk <hk.hrishi30@gmail.com>
2024-04-08 00:55:37 -0500

    feat: add vpc and subnet resources

--- main.tf
+++ main.tf
@@ -7,32 +7,32 @@
 
 # Syntax explanation: resource "specify the resource provider_required resource" "name of the resource locally for terraform to reference { actual name of the resource on aws. tags { which are metadata for our resource.}} tags allow us to organize our resources and provide additional information(metadata) about them.
 
-resource "aws_s3_bucket" "terraform-bucket" {
-  bucket = "my-terraform-s3-bucket-unique"
+# resource "aws_s3_bucket" "terraform-bucket" {
+#   bucket = "my-terraform-s3-bucket-unique"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
 
-# Now we want to create the aws cloudwatch log stream.
-# Step 1: Create the log group: collection container for log streams that share the same retention, monitoring, and access control settings.
-resource "aws_cloudwatch_log_group" "terra-log-group"{
-    name = "terraform-log-group-rishi"
-    retention_in_days = 30
+# # Now we want to create the aws cloudwatch log stream.
+# # Step 1: Create the log group: collection container for log streams that share the same retention, monitoring, and access control settings.
+# resource "aws_cloudwatch_log_group" "terra-log-group"{
+#     name = "terraform-log-group-rishi"
+#     retention_in_days = 30
 
-    tags = {
-      Name        = "My log group"
-      Environment = "Dev"
-    }
-}
+#     tags = {
+#       Name        = "My log group"
+#       Environment = "Dev"
+#     }
+# }
 
-# Step 2: Create the log stream: sequence of log events that share the same source.
-resource "aws_cloudwatch_log_stream" "terra-log-stream"{
-    name = "terra-log-stream-rishi"
-    log_group_name = aws_cloudwatch_log_group.terra-log-group.name # reference the log group name from the log group resource.
-}
+# # Step 2: Create the log stream: sequence of log events that share the same source.
+# resource "aws_cloudwatch_log_stream" "terra-log-stream"{
+#     name = "terra-log-stream-rishi"
+#     log_group_name = aws_cloudwatch_log_group.terra-log-group.name # reference the log group name from the log group resource.
+# }
 
 # Create an EC2 instance.
 resource "aws_instance" "terra-instance" {
@@ -44,10 +44,23 @@ resource "aws_instance" "terra-instance" {
   }
 }
 
+# A virtual private cloud is a virtual section of the cloud that is logically isolated from other virtual networks in the cloud.
+resource "aws_vpc" "terra-vpc" {
+  cidr_block = "10.0.0.0/16"
 
+  tags = {
+    Name = "devl"
+  }
+}
 
+# A subnet within a VPC is a range of IP addresses in the VPC. In other words it sections of our VPC which allow us to allocate IP addresses to our resources.
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.terra-vpc.id
+  cidr_block        = "10.0.1.0/24"
 
-
-
+  tags = {
+    Name = "devl-subnet"
+  }
+}
 
 
