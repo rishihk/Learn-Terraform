d68a6a1fe6494eaa83c162c3fdf0f62384d66c9f
rishihk <hk.hrishi30@gmail.com>
2024-04-08 01:38:04 -0500

    learn how to output configs in the console

--- main.tf
+++ main.tf
@@ -1,38 +1,45 @@
 # Part 2: Defining the services through resource blocks.
+
+# Running terraform init will initialize the terraform configuration.
 # Running terraform plan will show the changes that will be made to the infrastructure.
 # Running terraform apply will apply the changes to the infrastructure.
 # Running terraform destroy will destroy the infrastructure.
+# Running just terraform will show us the list of commands that we can run with terraform.
+# Running terraform state will show us a list of commands that we can run with terraform state, like list which will list all the resources in the state file. 
+# Running terraform state show <resource> will show us the details of the resource in the state file. (Current configuration).
+# --auto-approve flag will apply changes without asking for confirmation.
+# -target <resource> flag will destroy/apply only the specified resource.
 
 # I am creating an S3 bucket on my aws console.
 
 # Syntax explanation: resource "specify the resource provider_required resource" "name of the resource locally for terraform to reference { actual name of the resource on aws. tags { which are metadata for our resource.}} tags allow us to organize our resources and provide additional information(metadata) about them.
 
-# resource "aws_s3_bucket" "terraform-bucket" {
-#   bucket = "my-terraform-s3-bucket-unique"
+resource "aws_s3_bucket" "terraform-bucket" {
+  bucket = "my-terraform-s3-bucket-unique"
 
-#   tags = {
-#     Name        = "My bucket"
-#     Environment = "Dev"
-#   }
-# }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
 
-# # Now we want to create the aws cloudwatch log stream.
-# # Step 1: Create the log group: collection container for log streams that share the same retention, monitoring, and access control settings.
-# resource "aws_cloudwatch_log_group" "terra-log-group"{
-#     name = "terraform-log-group-rishi"
-#     retention_in_days = 30
+# Now we want to create the aws cloudwatch log stream.
+# Step 1: Create the log group: collection container for log streams that share the same retention, monitoring, and access control settings.
+resource "aws_cloudwatch_log_group" "terra-log-group"{
+    name = "terraform-log-group-rishi"
+    retention_in_days = 30
 
-#     tags = {
-#       Name        = "My log group"
-#       Environment = "Dev"
-#     }
-# }
+    tags = {
+      Name        = "My log group"
+      Environment = "Dev"
+    }
+}
 
-# # Step 2: Create the log stream: sequence of log events that share the same source.
-# resource "aws_cloudwatch_log_stream" "terra-log-stream"{
-#     name = "terra-log-stream-rishi"
-#     log_group_name = aws_cloudwatch_log_group.terra-log-group.name # reference the log group name from the log group resource.
-# }
+# Step 2: Create the log stream: sequence of log events that share the same source.
+resource "aws_cloudwatch_log_stream" "terra-log-stream"{
+    name = "terra-log-stream-rishi"
+    log_group_name = aws_cloudwatch_log_group.terra-log-group.name # reference the log group name from the log group resource.
+}
 
 # Create an EC2 instance.
 resource "aws_instance" "terra-instance" {
@@ -42,6 +49,12 @@ resource "aws_instance" "terra-instance" {
   tags = {
     Name = "terra-instance"
   }
+}
+
+# this will output the instance type of the instance that we created when we run terraform apply.
+# the .instance_type can be replaced with anything we see when we run terraform state show aws_instance
+output "instance-type"{ 
+  value = aws_instance.terra-instance.instance_type
 }
 
 # A virtual private cloud is a virtual section of the cloud that is logically isolated from other virtual networks in the cloud.
