a012d00e4251547c017a3650e1d2ae18884e3a38
rishihk <hk.hrishi30@gmail.com>
2024-04-07 21:29:50 -0500

    feat: create an ec2 instance thru terraform

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 # Running terraform destroy will destroy the infrastructure.
 # I am creating an S3 bucket on my aws console.
 
-# Syntax explanation: resource "specify the resource provider" "name of the resource locally for terraform to reference { actual name of the resource on aws. tags { which are metadata for our resource.}} tags allow us to organize our resources and provide additional information about them.
+# Syntax explanation: resource "specify the resource provider_required resource" "name of the resource locally for terraform to reference { actual name of the resource on aws. tags { which are metadata for our resource.}} tags allow us to organize our resources and provide additional information(metadata) about them.
 
 resource "aws_s3_bucket" "terraform-bucket" {
   bucket = "my-terraform-s3-bucket-unique"
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "terraform-bucket" {
 }
 
 # Now we want to create the aws cloudwatch log stream.
-# Step 1: Create the log group: collection contaainer for log streams that share the same retention, monitoring, and access control settings.
+# Step 1: Create the log group: collection container for log streams that share the same retention, monitoring, and access control settings.
 resource "aws_cloudwatch_log_group" "terra-log-group"{
     name = "terraform-log-group-rishi"
     retention_in_days = 30
@@ -32,3 +32,20 @@ resource "aws_cloudwatch_log_stream" "terra-log-stream"{
     name = "terra-log-stream-rishi"
     log_group_name = aws_cloudwatch_log_group.terra-log-group.name # reference the log group name from the log group resource.
 }
+
+# Create an EC2 instance.
+resource "aws_instance" "terra-instance" {
+  ami           = "ami-080e1f13689e07408" # must be region-specific! 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terra-ec2"
+  }
+}
+
+
+
+
+
+
+
